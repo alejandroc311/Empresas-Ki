d4161974a658c091d7dddbb747b135a061b7a094
alejandroc311 <alejandroc311@gmail.com>
2020-07-08 18:25:30 +0100

    Added some padding to carousel and made it responsive...Godspeed Julio!

--- public/carousel.js
+++ public/carousel.js
@@ -1,10 +1,21 @@
 new Glider(document.querySelector('.glider'), {
-  slidesToShow: 3,
+  slidesToShow: 1,
   slidesToScroll: 1,
   draggable: true,
   dots: '.dots',
   arrows: {
     prev: '.glider-prev',
     next: '.glider-next'
-  }
+  },
+  responsive: [
+   {
+     // screens greater than >= 775px
+     breakpoint: 775,
+     settings: {
+       // Set to `auto` and provide item width to adjust to viewport
+       slidesToShow: 3,
+       slidesToScroll: 1,
+     }
+   }
+ ]
 });
